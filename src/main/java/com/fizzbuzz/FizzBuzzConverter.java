3659810adb223154d7f715f715e920a1ffc0c987
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-07 13:24:46 +0530

    Added the test case to return for multiples of three as part of refactoring in business logic and in tets class.

--- src/main/java/com/fizzbuzz/FizzBuzzConverter.java
+++ src/main/java/com/fizzbuzz/FizzBuzzConverter.java
@@ -3,6 +3,8 @@ package com.fizzbuzz;
 class FizzBuzzConverter {
 
 	String convert(int number) {
+		if (number % 3 == 0)
+			return "Fizz";
 		return String.valueOf(number);
 	}
 }
--- src/test/java/com/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/fizzbuzz/FizzBuzzTest.java
@@ -2,13 +2,25 @@ package com.fizzbuzz;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FizzBuzzTest {
+	
+	private FizzBuzzConverter fizzBuzzConverter;
+	
+	@Before
+	public void initializeFizzBuzzInstance() {
+		fizzBuzzConverter = new FizzBuzzConverter();
+	}
 
 	@Test
 	public void fizzBuzzConverterShouldReturnOneFor1() {
-		FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
 		assertEquals("1", fizzBuzzConverter.convert(1));
+	}
+	
+	@Test
+	public void fizzBuzzConverterShouldReturnFizzForMultiplesOfThree() {
+		assertEquals("Fizz", fizzBuzzConverter.convert(3));
 	}
 }
