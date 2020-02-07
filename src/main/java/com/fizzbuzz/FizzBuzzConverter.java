2f414256b4cbecde4df1306fac386599c290b46f
Lokesh Varshney <var.lokesh@gmail.com>
2020-02-07 13:46:44 +0530

    Refactored the business logic by introducing three different methods which checks for Fizz, Buzz and FizzBuzz.

--- src/main/java/com/fizzbuzz/FizzBuzzConverter.java
+++ src/main/java/com/fizzbuzz/FizzBuzzConverter.java
@@ -7,12 +7,30 @@ class FizzBuzzConverter {
 	private static final String FizzBuzz = "FizzBuzz";
 
 	String convert(int number) {
-		if (number % (3 * 5) == 0)
+		if (isFizzBuzzNumber(number))
 			return FizzBuzz;
-		else if (number % 3 == 0)
+		else if (isFizzNumber(number))
 			return FIZZ;
-		else if(number % 5 == 0)
+		else if (isBuzzNumber(number))
 			return BUZZ;
 		return String.valueOf(number);
+	}
+	
+	private boolean isFizzNumber(int number) {
+		if (number % 3 == 0)
+			return true;
+		return false;
+	}
+
+	private boolean isBuzzNumber(int number) {
+		if (number % 5 == 0)
+			return true;
+		return false;
+	}
+
+	private boolean isFizzBuzzNumber(int number) {
+		if (number % (3 * 5) == 0)
+			return true;
+		return false;
 	}
 }
